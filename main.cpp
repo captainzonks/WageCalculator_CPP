50f64575fa9f2ad975cc08e6c1e351235c151e77
Matthew Barham <zonks.zonks@gmail.com>
2022-07-31 16:50:30 -0600

    improve: abstracted out functions in main.cpp

--- main.cpp
+++ main.cpp
@@ -2,18 +2,25 @@
 #include <iostream>
 #include <fstream>
 #include <sys/stat.h>
-#include <vector>
 
 #include "data.h"
 
 void ask_for_wage(data& in_data);
+
+void ask_to_change_wage(data& in_data);
+
+void ask_for_hours(data& in_data);
+
+void ask_for_tips(data& in_data);
+
 void print_pretty_data(data& in_data);
+
 
 int main() {
     data today_data{};
-    float wage{};
     bool exists{ false };
 
+    // Check if file exists, if not, ask to initialize wage
     struct stat buffer{};
     if (stat("wage_data", &buffer)) {
         ask_for_wage(today_data);
@@ -21,44 +28,39 @@ int main() {
         exists = true;
     }
 
-    std::fstream file("wage_data", std::fstream::in | std::fstream::out | std::fstream::app);
-
-    std::vector<data> data;
-    if (exists) {
-        while (!file.eof()) {
-            file >> today_data;
-            data.push_back(today_data);
-        }
-        data.pop_back();
-    }
-
-    file.close();
+    std::fstream file;
 
     file.open("wage_data", std::fstream::in | std::fstream::out | std::fstream::app);
 
-    char change_wage{'n'};
-    std::cout << "Would you like to change your wage from $" << today_data.get_wage() << "/hr? (Y or N) ";
-    std::cin >> change_wage;
-    if (std::tolower(change_wage) == 'y')
-    {
-        ask_for_wage(today_data);
+    // Retrieve the most recent data from the file
+    if (file) {
+        if (exists) {
+            while (!file.eof()) {
+                file >> today_data;
+            }
+        }
+    } else {
+        std::cout << "Error opening file";
+        file.close();
+        return 1;
     }
 
-    std::cout << "Please enter how many hours you worked today: ";
-    float hours{};
-    std::cin >> hours;
-    today_data.add_hours(hours);
-    today_data.set_today_hours(hours);
+    file.close();
+    file.open("wage_data", std::fstream::in | std::fstream::out | std::fstream::app);
 
-    std::cout << "Please enter your tips: ";
-    float tips{};
-    std::cin >> tips;
-    today_data.add_tips(tips);
-    today_data.set_today_tips(tips);
+    if (file) {
+        ask_to_change_wage(today_data);
+        ask_for_hours(today_data);
+        ask_for_tips(today_data);
 
-    file << today_data;
+        file << today_data;
 
-    print_pretty_data(today_data);
+        print_pretty_data(today_data);
+    } else {
+        std::cout << "Failed to open file";
+        file.close();
+        return 1;
+    }
 
     file.close();
 
@@ -70,6 +72,31 @@ void ask_for_wage(data& in_data) {
     std::cout << "Please enter your wage: ";
     std::cin >> wage;
     in_data.set_wage(wage);
+}
+
+void ask_to_change_wage(data& in_data) {
+    char change_wage{ 'n' };
+    std::cout << "Would you like to change your wage from $" << in_data.get_wage() << "/hr? (Y or N) ";
+    std::cin >> change_wage;
+    if (tolower(change_wage) == 'y') {
+        ask_for_wage(in_data);
+    }
+}
+
+void ask_for_hours(data& in_data) {
+    std::cout << "Please enter how many hours you worked today: ";
+    float hours{};
+    std::cin >> hours;
+    in_data.add_hours(hours);
+    in_data.set_today_hours(hours);
+}
+
+void ask_for_tips(data& in_data) {
+    std::cout << "Please enter your tips: ";
+    float tips{};
+    std::cin >> tips;
+    in_data.add_tips(tips);
+    in_data.set_today_tips(tips);
 }
 
 void print_pretty_data(data& in_data) {
